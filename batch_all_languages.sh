9bbd1b5f6e4237f2db843324099a04c569c431cf
Richard D Shipman <djshipman2011@gmail.com>
2025-03-16 16:19:52 +0100

    .

--- batch_all_languages.sh
+++ batch_all_languages.sh
@@ -4,11 +4,11 @@
 #LANG_CODES=("ZH" "ES" "EN" "HI" "AR" "PT" "BN" "RU" "JA" "PA" "DE" "FR" "VI" "MR" "TA" "UR" "TR" "IT" "FA" "PL" "UK" "RO" "NL" "TH" "HU" "EL" "HE" "ID" "MS" "KO" "SW" "FI" "CS" "DA" "SV" "SK" "BG" "HR" "SR" "SL" "LT" "LV" "ET" "CA" "EU" "GL" "GA" "CY" "MT" "IS" "MK" "HY" "BE" "SQ" "BS" "LB")
 
 LANG_CODES=(
-    # Most spoken languages
-    #"ZH" "ES" "CA" "EN" "HI" "AR" "PT" "BN" "RU" "JA" "PA" "DE" "FR" "VI" "MR" "TA" "UR" "TR" "IT" "FA" "PL" "UK" "RO" "NL" "TH" "HU" "EL" "HE" "ID" "MS" "KO" "SW" "FI" "CS" "DA" "SV" "SK" "BG" "HR" "SR" "SL" "LT" "LV" "ET" "EU" "GL" "GA" "CY" "MT" "IS" "MK" "HY" "BE" "SQ" "BS" "LB"
+    # Spoken languages
+     "ES" #"PT" "DE" "FR" "NL" "AR" "RU" "ZH" "JA" "TH" # "KO" "VI" "HI" "BN" "MR" "PA" "TA" "UR" "TR" "IT" "FA" "PL" "UK" "RO" "NL"  "HU" "EL" "HE" "ID" "MS" "KO" "SW" "FI" "CS" "DA" "SV" "SK" "BG" "HR" "SR" "SL" "LT" "LV" "ET" "EU" "GL" "GA" "CY" "MT" "IS" "MK" "HY" "BE" "SQ" "BS" "LB"
     
     # Fantasy and Sci-Fi languages
-    "ELV" "DWV" "PRT" "HVL" "DTH" "KLN" "OTL" "NAV" "BJR" "HTS" "VLC" "ESP" "SML" "ATL" "MNS" "VKR" "PIR" "TSL" "LOTR" "GND" "YDI" "SNO"
+    #"ELV" "DWV" "PRT" "HVL" "DTH" "KLN" "OTL" "NAV" "BJR" "HTS" "VLC" "ESP" "SML" "ATL" "MNS" "VKR" "PIR" "TSL" "LOTR" "GND" "YDI" "SNO"
 
     # Emoji and Brain Rot languages
     #"EMJ" "BRR"
@@ -16,7 +16,7 @@ LANG_CODES=(
 )
 
 # Define input file (fantasy language plus+++)
-INPUT_FILE="test_data/input.csv"
+INPUT_FILE="export.csv"
 
 # Define Ollama hosted model
 #MODEL="llama3.2:latest"  # Adjust if needed
@@ -27,7 +27,7 @@ MODEL="llama3.2:1b"
 #MODEL="deepseek-r1:1.5b"
 
 # Define columns to translate
-COLUMNS=("column1" "column2")  # Change to the columns you want to translate
+COLUMNS=("name")  # Change to the columns you want to translate
 
 # Loop through all language codes and run the Python script
 for LANG in "${LANG_CODES[@]}"; do
